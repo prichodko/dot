3de62ae837e8fe88bbb418e3491b9bc06f9f5266
pavel <14926950+prichodko@users.noreply.github.com>
2026-01-21 14:04:52 +0100

    chore: use ~/.dotfiles

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,8 +2,8 @@ import { homedir } from "os";
 import { join } from "path";
 
 export const DOTFILES_REPO = "prichodko/dotfiles";
-export const DOTFILES_DIR = join(homedir(), ".config", "dotfiles");
-export const BACKUP_DIR = join(homedir(), ".config", "dotfiles-backup");
+export const DOTFILES_DIR = join(homedir(), ".dotfiles");
+export const BACKUP_DIR = join(homedir(), ".dotfiles-backup");
 export const HOME = homedir();
 
 export interface Config {
